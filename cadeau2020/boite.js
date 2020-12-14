eff9373ba24060babdd01651466a4b174108dac8
Veronika Kolokolnikova <vero46.kolo@gmail.com>
2020-12-14 17:26:42 +0100

    initial commit

---
+++ cadeau2020/boite.js
@@ -0,0 +1,86 @@
+function afficherBlague()
+{
+  alert("Un gars rencontre un de ses copain, ce dernier est pas mal amoché, il a un œil et une joue bleue. \n Son copain lui demande:\n- Qu'est qu'il t'arrive mon ami?\n Le gars lui répond:\n- J'ai reçu un coup au visage.\n- Qui t'as fait ça?\n- Ma femme.\n- Ta femme? Quand? Comment? Pourquoi?\n- Je suis derrière ma femme, qui porte une jupe courte, qui se penche dans le congélateur devant moi. Je vois apparaître le string et la boule de fesse, je perds le contrôle, j'agrippe ma femme, je tasse le string et je la prends par derrière. Ce que je me souviens c'est d'avoir reçu un coup de jarret de porc congelé au visage.- Est-ce que c'était la première fois que tu faisais ça à ta femme?\n- Au supermarché, oui.");
+}
+function afficherInvitation()
+{
+ alert("Invitation dans le restaurant de ton choix dès la fin du confinement");
+}
+function cinema()
+{
+ alert("C'est toi ma star <3 ");
+}
+function escapegame()
+{
+	window.open("https://jeux.eludice.com/escape-japanese/");
+	
+}
+function qse()
+{
+	window.open("https://www.previnfo.net/modules.php?ModPath=quiz&ModStart=quiz&categ=7");
+	
+}
+function sport()
+{
+	window.open("https://www.lotus-bouche-cousue.fr/wp-content/uploads/2018/02/PROGRAMME-BINOME-COUPLE-LOTUS-BOUCHE-COUSUE2-1.pdf");
+}
+function musique()
+{
+	window.open("https://www.youtube.com/watch?v=vKPSTH-r2ZI");	
+}
+function netflix()
+{
+	window.open("https://flickmetrix.com/");
+}
+function biere()
+{
+		window.open("https://www.happybeertime.com/blog/2013/09/04/top-20-bieres-insolites-bizarres-2/");
+}
+function bricolage()
+{
+	window.open("https://www.youtube.com/watch?v=HA0WJnFiaLE");
+}
+function om()
+{
+	window.open("https://www.youtube.com/watch?v=5Hpd-dOgLWc");
+}
+function jeux()
+{
+	window.open("https://fr.asmodee.com/fr/news/2020/3/27/pack-jeux/");
+}
+function voyages()
+{
+	window.open("https://www.businessinsider.fr/100-voyages-a-faire-dans-sa-vie-selon-experts-voyage");
+}
+function livre()
+{
+	window.open("https://www.flair.be/fr/culture/livres/les-100-livres-a-lire-dans-sa-vie/");
+}
+function lapin()
+{
+	window.open("http://www.nomdelapin.fr/quiz-lapin-difficile.php");
+}
+function pompiers()
+{
+	window.open("https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/78246780_525163458067301_3094824133507153920_o.jpg?_nc_cat=105&_nc_sid=110474&_nc_oc=AQnIQcd4hWOYNgaFuX0T0AhcAjHQZNszvzNd_cS5JWXUVQoYfI16GI4WQoPlz1PMggw&_nc_ht=scontent-cdt1-1.xx&_nc_tp=6&oh=7df4db5932881d54927ee17293f5468f&oe=5EC2F525");
+}
+function casino()
+{
+	window.open("https://www.youtube.com/watch?v=uAghq3GPeM4");
+}
+function voitures()
+{
+	window.open("http://www.voituresdelegende.com/");
+}
+function renard()
+{
+	window.open("https://www.youtube.com/watch?v=Y3j7Ayo-jig");
+}
+function nature()
+{
+	window.open("https://www.youtube.com/watch?v=fm5kiVEklGA");
+}
+function environnement()
+{
+	window.open("https://www.linternaute.com/sortir/nature/1070528-les-100-plus-beaux-paysages-de-france/");
+}
